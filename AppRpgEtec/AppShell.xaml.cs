d65f46b2457a44ba7b7e574ff0b47182cd0d57bb
Vitor <vitorsouzalibrelonrestini@gmail.com>
2023-10-16 12:02:16 -0300

    Projeto front com  as abas, footer, header, etec

---
+++ AppRpgEtec/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using AppRpgEtec.Views.Personagens;
+
+namespace AppRpgEtec;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+
+		string login = Preferences.Get("UsuarioUsername", string.Empty);
+		lblLogin.Text = $"login: {login}";
+	}
+}
+
+
